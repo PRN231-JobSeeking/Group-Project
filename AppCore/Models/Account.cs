f9ff1aae5504c5bd65932ab563acf3a0e4e148a8
WilliamTrung <williamthanhtrungq2@gmail.com>
2023-02-12 20:49:52 +0700

    add_password_t_account

--- AppCore/Models/Account.cs
+++ AppCore/Models/Account.cs
@@ -12,6 +12,7 @@ namespace AppCore.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; } 
         public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
         public string FirstName { get; set; } = null!;
         public string LastName { get; set; } = null!;
         public string Address { get; set; } = null!;
